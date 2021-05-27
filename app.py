339dcedf1be60007aebe3d4dab83d1e21ff2df4a
Naveen Fernando <malinda0610@gmail.com>
2021-05-27 16:44:55 +0530

    first commit

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask,render_template, request, jsonify
 from flask_compress import Compress
 
 # initialize flask application
-app = Flask(__name__,static_url_path='',static_folder='dist',template_folder='dist')
+app = Flask(__name__)
 Compress(app)
-
 
 @app.route('/')
 def test():
     return render_template("index.html")
+
+# @app.route('/')
+# def root():
+#     return app.send_static_file('index.html')
 
 @app.route('/flask/hello')
 def testing():
