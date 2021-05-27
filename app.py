220eac4ef82b4a1f27834ae3203e73146524be90
Naveen Fernando <malinda0610@gmail.com>
2021-05-27 15:59:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask,render_template, request, jsonify
+from flask_compress import Compress
+
+# initialize flask application
+app = Flask(__name__,static_url_path='',static_folder='dist',template_folder='dist')
+Compress(app)
+
+
+@app.route('/')
+def test():
+    return render_template("index.html")
+
+@app.route('/flask/hello')
+def testing():
+  return "Hellow from heroku"
+
+if __name__ == '__main__':
+    # run web server
+    app.run()
+
